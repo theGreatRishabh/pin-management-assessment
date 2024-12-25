497a03c28eed4453fc103db8252a3336f5bc053b
theGreatRishabh <rk.rishabh.k@gmail.com>
2024-12-25 16:26:56 +0530

    implement customer management with local storage; add customer form and display table; update pin form to load customers dynamically

--- src/app/shared/pin-form/pin-form.component.ts
+++ src/app/shared/pin-form/pin-form.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { CustomerService } from 'src/app/services/customer.service';
+import { PinService } from 'src/app/services/pin.service';
 
 @Component({
   selector: 'app-pin-form',
@@ -8,42 +10,50 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 })
 export class PinFormComponent implements OnInit {
   pinForm: FormGroup;
-  customers: string[] = [
-    'Customer 1',
-    'Customer 2',
-    'Customer 3',
-    'Customer 4',
-    'Customer 5',
-  ];
+  customers: any[] = [];
   selectedFile: File | null = null;
   fileTouched = false;
 
   @Output() pinAdded = new EventEmitter<any>();
 
-  constructor(private fb: FormBuilder) {
+  constructor(
+    private fb: FormBuilder,
+    private customerService: CustomerService,
+    private pinService: PinService,
+  ) {
     this.pinForm = this.fb.group({
       title: ['', Validators.required],
-      image: ['', Validators.required],
       collaborators: ['', Validators.required],
-      privacy: ['Public', Validators.required],
+      file: ['', Validators.required],
+      privacy: ['', Validators.required],
     });
   }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.loadCustomers();
+  }
+
+  loadCustomers() {
+    this.customers = this.customerService.getCustomers();
+  }
 
   onFileChange(event: Event): void {
     const input = event.target as HTMLInputElement;
     if (input.files && input.files.length > 0) {
       this.selectedFile = input.files[0];
-      this.pinForm.patchValue({ image: this.selectedFile.name });
+      this.fileTouched = true;
     }
-    this.fileTouched = true;
   }
 
   onSubmit(): void {
     if (this.pinForm.valid) {
-      this.pinAdded.emit(this.pinForm.value);
-      console.log(this.pinForm.value);
+      const pinData = this.pinForm.value;
+      pinData.file = this.selectedFile;
+      const pins = JSON.parse(localStorage.getItem('pins') || '[]');
+      pins.push(pinData);
+      localStorage.setItem('pins', JSON.stringify(pins));
+      this.pinAdded.emit(pinData);
+      console.log('Pin added:', pinData);
     }
   }
 }
